9461020334771c6bd1797f2947f699347c7d4e2d
ShAzzEmatkasse <65095487+ShAzzEmatkasse@users.noreply.github.com>
2021-01-18 14:02:41 +0100

    fd
    df

---
+++ ja.py
@@ -0,0 +1,1 @@
+print("One is the loneliest number that you'll ever do Two can be as bad as oneIt's the loneliest number since the number oneNo is the saddest experience you'll ever knowYes is the saddest experience you'll ever kno'Cause one is the loneliest number that you'll ever knowOne is the loneliest number even worse than two, yeah")

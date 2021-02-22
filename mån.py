8a3acb49270782bf4514dc33ef37216988d6e474
ShAzzEmatkasse <65095487+ShAzzEmatkasse@users.noreply.github.com>
2021-02-22 14:07:27 +0100

    df
    df

---
+++ mån.py
@@ -0,0 +1,20 @@
+
+while True:
+    losenord = input ('Skriv ditt lösenord')   
+    if losenord == "Hej!":
+        print("Rätt lösenord!")
+        break
+    else:
+        print('Fel lösenord!')
+
+
+[1] = visa alla loggar 
+[2] = skriv en ny logg
+[3] = rensa alla loggar
+[4] = avsulta
+dropdown = input ([1] = visa alla loggar 
+[2] = skriv en ny logg
+[3] = rensa alla loggar
+[4] = avsulta
+
+)
